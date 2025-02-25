d884e411e58ef7c8b671036e458ae1975bffd324
Adrian Schmitz <a.schmitz@itc.rwth-aachen.de>
2025-02-25 12:47:17 +0100

    Add data optimized benchmarks

---
+++ simple_examples/01MxM_load_count_Dataopt.cpp
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <omp.h>
+#include <mpi.h>
+#include <string>
+
+
+int main(int argc, char** argv) {
+
+    int rank, size;
+    int provided;
+    int device;
+
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	
+	// targetDART initialization
+    td_init((void *) &main);
+
+    if (std::getenv("MODE") == NULL) {
+        device = TARGETDART_ANY;
+    } else {
+
+        std::string mode = std::getenv("MODE");
+        if (mode == "CPU") {
+            device = TARGETDART_CPU;
+        } else if (mode == "GPU") {
+            device = TARGETDART_OFFLOAD;
+        } else {
+            device = TARGETDART_ANY;
+        }
+    }
+
+    //std::cout << "MPI size, rank: " << size << ", " << rank << std::endl;
+
+    if (argc < 4 + size) {
+        std::cerr << "not enough arguments: For " << size << " MPI processes you need at least " << 4 + size << " Arguments" << std::endl;
+        exit(1);
+    }
+    
+    int d1 = std::atoi(argv[1]);
+    int d2 = std::atoi(argv[2]);
+    int d3 = std::atoi(argv[3]);
+    
+    int iter = std::atoi(argv[rank + 4]);
+
+    double * A = (double*) malloc(d1 * d2 * sizeof(double));
+    double * B = (double*) malloc(d2 * d3 * sizeof(double));
+    double * C = (double*) malloc(d1 * d3 * sizeof(double));
+
+    for (int i = 0; i < d1 * d2; i++) {
+        A[i] = 1;
+    }
+    for (int i = 0; i < d2 * d3; i++) {
+        B[i] = 1;
+    }
+    for (int i = 0; i < d1 * d3; i++) {
+        C[i] = 0;
+    }
+    
+    double time = omp_get_wtime();   
+    #pragma omp target data map(to:A[0:d1*d2],B[0:d2*d3],d1,d2,d3) device(device)
+    {
+    for (int l = 0; l < iter; l++) {
+        #pragma omp target teams distribute parallel for map(from:C[0:d1*d3]) device(device) collapse(2) nowait
+        for (int i = 0; i < d1; i++) {
+            for (int k = 0; k < d3; k++) {
+                C[i * d3 + k] = 0;
+                for (int j = 0; j < d2; j++) {                
+                    C[i * d3 + k] += A[i * d2 + j] * B[j * d3 + k];
+                }   
+            }
+        }
+    }    
+    #pragma omp taskwait
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    time = omp_get_wtime() - time;
+    
+    if (rank == 0) {
+        std::cout << "duration on process " << rank << ": " << time << std::endl;
+        //std::cout << "Result:  " << C[0] << std::endl;
+    }
+    
+    free(A);
+    free(B);
+    free(C);
+    
+    //finalizeTargetDART();
+    MPI_Finalize();
+    return 0;
+}
