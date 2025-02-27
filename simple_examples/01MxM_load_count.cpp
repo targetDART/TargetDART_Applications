0ff8f15e77b81994b80392ab54eb5f00fb9f2ac2
Jan Kraus <kraus@itc.rwth-aachen.de>
2025-02-27 15:09:24 +0100

    Added pinned memory to 01MxM_load_count.cpp

--- simple_examples/01MxM_load_count.cpp
+++ simple_examples/01MxM_load_count.cpp
@@ -5,7 +5,6 @@
 #include <mpi.h>
 #include <string>
 
-
 int main(int argc, char** argv) {
 
     int rank, size;
@@ -43,12 +42,35 @@ int main(int argc, char** argv) {
     int d1 = std::atoi(argv[1]);
     int d2 = std::atoi(argv[2]);
     int d3 = std::atoi(argv[3]);
-    
+
+
     int iter = std::atoi(argv[rank + 4]);
 
-    double * A = (double*) malloc(d1 * d2 * sizeof(double));
-    double * B = (double*) malloc(d2 * d3 * sizeof(double));
-    double * C = (double*) malloc(d1 * d3 * sizeof(double));
+    // enable paged memory only when PAGED is explcitly enabled
+    bool paged = false;
+    if (auto *paged_env = std::getenv("PAGED")) {
+        std::string paged_str = paged_env;
+        paged = !(paged_str == "0" || paged_str.empty());
+    }
+
+    double *A, *B, *C;
+    int *d;
+
+    if (paged) {
+        A = (double*) malloc(d1 * d2 * sizeof(double));
+        B = (double*) malloc(d2 * d3 * sizeof(double));
+        C = (double*) malloc(d1 * d3 * sizeof(double));
+        d = (int*) malloc(3 * sizeof(int));
+    } else {
+        A = (double*)omp_alloc(d1 * d2 * sizeof(double), llvm_omp_target_host_mem_alloc);
+        B = (double*)omp_alloc(d2 * d3 * sizeof(double), llvm_omp_target_host_mem_alloc);
+        C = (double*)omp_alloc(d1 * d3 * sizeof(double), llvm_omp_target_host_mem_alloc);
+        d = (int*)omp_alloc(3*sizeof(int), llvm_omp_target_host_mem_alloc);
+    }
+
+    d[0] = d1;
+    d[1] = d2;
+    d[2] = d3;
 
     for (int i = 0; i < d1 * d2; i++) {
         A[i] = 1;
@@ -63,12 +85,12 @@ int main(int argc, char** argv) {
     double time = omp_get_wtime();   
     
     for (int l = 0; l < iter; l++) {
-        #pragma omp target teams distribute parallel for map(from:C[0:d1*d3]) map(to:A[0:d1*d2]) map(to:B[0:d2*d3]) map(to:d1,d2,d3) device(device) collapse(2) nowait
-        for (int i = 0; i < d1; i++) {
-            for (int k = 0; k < d3; k++) {
-                C[i * d3 + k] = 0;
-                for (int j = 0; j < d2; j++) {                
-                    C[i * d3 + k] += A[i * d2 + j] * B[j * d3 + k];
+        #pragma omp target teams distribute parallel for map(from:C[0:d1*d3]) map(to:A[0:d1*d2]) map(to:B[0:d2*d3]) map(to:d[0:3]) device(device) collapse(2) nowait
+        for (int i = 0; i < d[0]; i++) {
+            for (int k = 0; k < d[2]; k++) {
+                C[i * d[2] + k] = 0;
+                for (int j = 0; j < d[1]; j++) {
+                    C[i * d[2] + k] += A[i * d[1] + j] * B[j * d[2] + k];
                 }   
             }
         }
@@ -83,11 +105,19 @@ int main(int argc, char** argv) {
         std::cout << "duration on process " << rank << ": " << time << std::endl;
         //std::cout << "Result:  " << C[0] << std::endl;
     }
-    
-    free(A);
-    free(B);
-    free(C);
-    
+
+    if (paged) {
+        free(A);
+        free(B);
+        free(C);
+        free(d);
+    } else {
+        omp_free(A, llvm_omp_target_host_mem_alloc);
+        omp_free(B, llvm_omp_target_host_mem_alloc);
+        omp_free(C, llvm_omp_target_host_mem_alloc);
+        omp_free(d, llvm_omp_target_host_mem_alloc);
+    }
+
     //finalizeTargetDART();
     MPI_Finalize();
     return 0;
